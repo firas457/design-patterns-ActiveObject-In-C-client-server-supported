3c287337c3101768499b7a7d0b64870991fbcfff
Firas <97015893+firas457@users.noreply.github.com>
2022-06-05 12:01:08 +0300

    Add files via upload

---
+++ part1_final/imple.hpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <mutex>
+using namespace std;
+
+class node
+{
+public:
+    node *next;
+    int data;
+
+    node(int& data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+class queue
+{
+
+    pthread_mutex_t lock;
+
+private:
+    node *head = NULL;
+
+public:
+    queue()
+    {
+    }
+
+    void enQ(int new_node)
+    {
+        pthread_mutex_lock(&lock);
+        // char data[1024];
+        // data = (std::string)new_node;
+        // std::string &data = *(static_cast<std::string*>(new_node));
+        node * new_node1=new node(new_node);
+
+   
+        node *temp = NULL;
+
+        if (head == NULL)
+        {
+            head = new_node1;
+            pthread_mutex_unlock(&lock);
+         
+        }
+
+        else
+        {
+            temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+
+            new_node1->next = NULL;
+            temp->next = new_node1;
+            pthread_mutex_unlock(&lock);
+    
+        }
+    }
+
+    void deQ()
+    {
+             pthread_mutex_lock(&lock);
+      
+        if (empty())
+        {
+            std::__throw_out_of_range("there is no elements");
+        }
+
+        else
+        {
+            head = head->next;
+            pthread_mutex_unlock(&lock);
+   
+        }
+    }
+
+    int size()
+    {
+        node *temp;
+        temp = head;
+
+        if (temp == NULL)
+        {
+            return 0;
+        }
+
+        int size = 1;
+
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+            size++;
+        }
+
+        return size;
+    }
+
+    bool empty()
+    {
+
+        bool ans = true;
+
+        if (head == NULL)
+        {
+            return ans;
+        }
+        return (!ans);
+    }
+
+    void print()
+    {
+        node *temp;
+        temp = head;
+
+        while (temp != NULL)
+        {
+            std::cout << temp->data << "-------->";
+            temp = temp->next;
+        }
+    }
+};
+
+class Queue
+{
+    queue *queue_;
+    pthread_mutex_t lock2;
+
+public:
+    void createQ()
+    {
+        
+        queue_ = new queue();
+   
+    }
+    void destroyQ()
+    {
+       
+        delete queue_;
+       
+    }
+    void enQ(int new_node)
+    {
+        queue_->enQ(new_node);
+    }
+    void deQ()
+    {
+        queue_->deQ();
+    }
+    void print()
+    {
+        queue_->print();
+    }
+
+    int size()
+    {
+        return queue_->size();
+    }
+};
+
